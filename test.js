8cf089937286a98195718c3f36e69ed46b41b889
VScode-Alice <sauce404k@gmail.com>
2026-01-16 14:48:57 +0800

    添加test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+// test.js
+console.log("Hello from test.js!");
+console.log("This is a test file added via VS Code.");
+
+// 测试函数
+function add(a, b) {
+    return a + b;
+}
+
+console.log("2 + 3 =", add(2, 3));
